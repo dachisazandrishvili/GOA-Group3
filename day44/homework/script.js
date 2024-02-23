4b15651b3ed1944cc0e1a5d36344b4c076ad34b0
dachisazandrishvili <146382038+dachisazandrishvili@users.noreply.github.com>
2024-02-23 13:02:11 +0400

    Add files via upload

---
+++ day44/homework/script.js
@@ -0,0 +1,9 @@
+
+let num1 = Number(prompt("enter num1 "))
+    let num2 = Number(prompt("enter num2 "))
+    let num3 = Number(prompt("enter num3 "))
+    function sum(){
+      umm = num1 + num2 + num3;
+      alert("sum is " + umm)
+    }
+sum();
